9fd736625405d1e7ffd442140c6dd9dde2fe68d0
Daniel Israel <danny.israel@gmail.com>
2025-10-22 15:58:29 -0700

    Convex and nativeUI

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,21 @@
-import { Stack } from "expo-router";
-import "../global.css";
+import { ThemeProvider, useTheme } from '@/lib/theme-context';
+import { Stack } from 'expo-router';
+import { View } from 'react-native';
+import '../global.css';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ThemeProvider defaultTheme="system">
+      <AppContent />
+    </ThemeProvider>
+  );
+}
+
+function AppContent() {
+  const { activeTheme } = useTheme();
+  return (
+    <View style={activeTheme} className="flex-1 bg-background">
+      <Stack />
+    </View>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View
-      className="flex-1 justify-center items-center"
+      className="flex-1 justify-center items-center gap-4"
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
